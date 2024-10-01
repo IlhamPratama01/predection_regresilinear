af9538c32952a95b2249c75e7b9986bac0671a28
IlhamPratama01 <muhamadilhampratama2@gmail.com>
2024-10-01 11:41:41 +0700

    prediction

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
 import pickle
+
+# Running Project Html = uvicorn main:app --reload
 
 app = FastAPI()
 
-# Definisikan model input
-class HouseData(BaseModel):
-    LB: float
-    LT: float
-    KT: int
-    KM: int
-    GRS: int
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 # Muat model yang sudah disimpan
-with open('lin_reg_model.pkl', 'rb') as f:
+with open('lin_reg_rumah.pkl', 'rb') as f:
     lin_reg = pickle.load(f)
 
-@app.put("/predict")
-def update_item(data: HouseData):
-    # Buat prediksi
-    prediction = lin_reg.predict([[data.LB, data.LT, data.KT, data.KM, data.GRS]])
-    harga_prediksi = round(prediction[0], 0)
-    
-    return {"predicted_price": harga_prediksi}
+@app.post("/predict", response_class=HTMLResponse)
+async def create(request: Request, pc: int = Form('pc'), cf: int = Form('cf'), yb: int = Form('yb'), ad: int = Form('ad'), ls: float = Form('ls'), br: int = Form('br')):
+    # Konversi nilai dari string menjadi tipe numerik
 
-# Endpoint root untuk pengujian
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    PC = int(pc)
+    CF = int(cf)
+    YB = int(yb)
+    AD = int(ad)
+    LS = float(ls)
+    BR = int(br)
+    
+    prediction = lin_reg.predict([[PC, CF, YB, AD, LS, BR]])
+    harga_prediksi = round(prediction[0], 0)
+
+    if harga_prediksi < 0:
+        harga_prediksi = -harga_prediksi
+
+    formatted_harga_prediksi = "{:,.0f}".format(harga_prediksi)
+
+
+    # Render template HTML dengan data prediksi
+    return templates.TemplateResponse("index.html", {"request": request, "predicted_price": formatted_harga_prediksi, "tahun":yb, "luas":ls})
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
